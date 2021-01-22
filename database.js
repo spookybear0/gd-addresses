d9b6f7da1840fa043f4be5bdc6740e2af32944c7
HJfod <dreadrollmusic@gmail.com>
2021-01-22 13:43:07 +0200

    ye p much

--- database.js
+++ database.js
@@ -1,7 +1,68 @@
+// this is the list of all the addresses
+// (i have no clue how to access a json file on github pages,
+// so for now, i'm using this instead.)
+
+// use EditLevelLayer as a reference for how to add a new address.
+// rest is handled automatically
+
 const addresses = [
     {
-        GDfunction: "EditLevelLayer::init",
-        address: "",
-        tags: [ "Local", "Level", "View", "Share", "Publish" ]
+        namespace: "EditLevelLayer",
+        contents: [
+            {
+                function: "init",
+                address: "%GD + 0x6F5D0",
+                tags: [ "Local", "Level", "View", "Share", "Publish" ],
+                args: [
+                    { arg: "-returnValue", type: "signed int", desc: "Unknown" },
+                    { arg: "self", type: "CCLayer*", desc: "The initialized layer itself" },
+                    { arg: "unknown", type: "void*", desc: "Unknown, required by calling convention" },
+                    { arg: "GameLevel", type: "GameLevel*", desc: "The level used to initialize the layer" },
+                ],
+                callingConvention: "__fastcall",
+                description: "The initialization function for EditLevelLayer"
+            }
+        ]
+    },
+    {
+        namespace: "FLAlertLayer",
+        contents: [
+            {
+                function: "create",
+                address: "%GD + 0x227E0",
+                tags: [],
+                callingConvention: "",
+                description: "Popup create"
+            }
+        ]
+    },
+    {
+        namespace: "GameManager",
+        contents: [
+            {
+                function: "init",
+                address: "%GD + 0x3222D0"
+            },
+            {
+                function: "getSharedState",
+                address: "%GD + 0xC4A50"
+            },
+            {
+                function: "getGameVariable",
+                address: "%GD + 0xC9D30"
+            },
+            {
+                function: "PlayLayer",
+                address: "%GM + 0x164"
+            },
+            {
+                function: "EditorLayer",
+                address: "%GM + 0x168"
+            },
+            {
+                function: "userName",
+                address: "%GM + 0x198"
+            }
+        ]
     }
 ];
--- index.js
+++ index.js
@@ -1,16 +1,116 @@
-document.querySelectorAll("nav button").forEach(b => {
-    b.addEventListener('click', e => {
-        document.querySelectorAll(".nav-selected").forEach(bb => {
-            bb.classList.remove('nav-selected');
+const searchInput = document.querySelector("#search input");
+const articles = document.querySelector("#content");
+
+function convertAddr(str) {
+    return str.replace(/%GD/g, "GeometryDash.exe").replace(/%GM/g, "<a href=''>GameManager</a>")
+}
+
+function generateFunctionText(ns, addr) {
+    if (!addr.args) return "No argument information available";
+    let resFunc =   "<t-main>" +
+                    addr.callingConvention +
+                    "</t-main> <t-third>" +
+                    ns.namespace +
+                    "</t-third>::<t-sec>" +
+                    addr.function +
+                    "</t-sec>(";
+    let resExpl = "";
+    addr.args.forEach(a => {
+        switch (a.arg) {
+            case "-returnValue":
+                resFunc = "<t-fourth>" + a.type + "</t-fourth> " + resFunc;
+                resExpl = "<tr><td><t-main>returnValue</t-main></td><td>" + a.type + "</td><td>" + a.desc + "</td></tr>\n" + resExpl;
+                break;
+            default:
+                resFunc += "<t-fourth>" + a.type + "</t-fourth> " + a.arg + ", ";
+                resExpl += "<tr><td><t-main>" + a.arg + "</t-main></td><td>" + a.type + "</td><td>" + a.desc + "</td></tr>\n";
+        }
+    });
+    resFunc = resFunc.substr(0, resFunc.length - 2) + ")";
+
+    console.log(resExpl);
+
+    return `
+        <text><t-code>${resFunc}</t-code></text>
+        <margin-top></margin-top>
+        <margin-top></margin-top>
+        <table>
+            <tr>
+                <th>Parameter</th>
+                <th>Type</th>
+                <th>Description</th>
+            </tr>
+            ${resExpl}
+        </table>
+    `;
+}
+
+function generateFunctionDesc(addr) {
+    if (addr.description)
+        return `
+            <margin-top />
+            <text><t-sec>${addr.description}</t-sec></text>
+        `;
+    else return ``;
+}
+
+addresses.forEach((ns, nix) => {
+    ns.contents.forEach((add, aix) => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+        <h1>${ns.namespace}::${add.function}</h1>
+        <text>${convertAddr(add.address)}</text>
+        <section>
+            ${generateFunctionText(ns, add)}
+            ${generateFunctionDesc(add)}
+        </section>
+        <a-meta data-meta='${nix}::${aix}'></a-meta>
+        `;
+
+        article.addEventListener('click', e => {
+            if (article.querySelector('section:hover'))
+                return;
+            const d = article.querySelector('section');
+            if (d.style.display == 'none') {
+                d.style.display = 'flex';
+                article.classList.add('is-open');
+            } else {
+                d.style.display = 'none';
+                article.classList.remove('is-open');
+            }
         });
-        b.classList.add('nav-selected');
+        article.querySelector('section').style.display = 'none';
+    
+        articles.append(article);
     });
 });
 
-const searchInput = document.querySelector("#search input");
+// this definitely needs to be improved, and like, a lot
+function searchFunc(data, aix, searchString) {
+    let found = 0;
+    const keywords = searchString.replace(/\:/g, " ").trim().split(" ");
+
+    // search by literally just turning the data into a json string and checking
+    // if that string contains what was searched lmfaoo
+    keywords.forEach(s => {
+        found += JSON.stringify(data.contents[aix]).toLowerCase().includes(s.toLowerCase())
+                || data.namespace.toLowerCase().includes(s.toLowerCase());
+    });
+    return found == keywords.length;
+}
 
 searchInput.addEventListener('input', e => {
-    
+    document.querySelectorAll("#content article").forEach(art => {
+        const ixs = art.querySelector("a-meta").getAttribute("data-meta").split("::");
+        const nix = parseInt(ixs[0]);
+        const aix = parseInt(ixs[1]);
+
+        if (searchFunc(addresses[nix], aix, searchInput.value))
+            art.style.display = "initial";
+        else
+            art.style.display = "none";
+    });
 });
 
-feather.replace();
+if (feather)
+    feather.replace();
