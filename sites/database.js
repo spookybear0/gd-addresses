1278f20e00f17490afa26955495a7f118f36db57
HJfod <dreadrollmusic@gmail.com>
2021-01-24 00:24:01 +0200

    improved stuff, added PlayLayer

--- sites/database.js
+++ sites/database.js
@@ -73,17 +73,121 @@ const addresses = [
                 address: "%GD + 0xC9D30"
             },
             {
-                function: "PlayLayer",
+                function: ".PlayLayer",
                 address: "%GM + 0x164"
             },
             {
-                function: "EditorLayer",
+                function: ".EditorLayer",
                 address: "%GM + 0x168"
             },
             {
-                function: "userName",
+                function: ".userName",
                 address: "%GM + 0x198"
             }
+        ]
+    },
+    {
+        namespace: "PlayLayer",
+        contents: [
+            {
+                function: "create",
+                address: "%GD + 0x1FB6D0"
+            },
+            {
+                function: "init",
+                address: "%GD + 0x01FB780"
+            },
+            {
+                function: "update",
+                address: "%GD + 0x2029C0"
+            },
+            {
+                function: "updateReplay",
+                address: "%GD + 0x20AF40"
+            },
+            {
+                function: "releaseButton",
+                address: "%GD + 0x1F4F70"
+            },
+            {
+                function: "pushButton",
+                address: "%GD + 0x111500"
+            },
+            {
+                function: ".Player2",
+                address: "PlayLayer + 0x228"
+            },
+            {
+                function: ".Player1",
+                address: "PlayLayer + 0x224"
+            },
+            {
+                function: ".time",
+                address: "PlayLayer + 0x450"
+            },
+            {
+                function: "onQuit",
+                address: "%GD + 0x20D810"
+            },
+            {
+                function: "levelComplete",
+                address: "%GD + 0x1FD3D0"
+            },
+            {
+                function: ".isDead",
+                address: "PlayLayer + 0x39C"
+            },
+            {
+                function: ".levelLength",
+                address: "PlayLayer + 0x3B4"
+            },
+            {
+                function: ".isPractice",
+                address: "PlayLayer + 0x495"
+            },
+            {
+                function: ".currentAttempt",
+                address: "PlayLayer + 0x4A8"
+            },
+            {
+                function: "timeForXPos2",
+                address: "%GD + 0x1FD3D0"
+            },
+            {
+                function: "togglePracticeMode",
+                address: "%GD + 0x20D0D1"
+            },
+            {
+                function: "destroyPlayer",
+                address: "%GD + 0x20A1A0"
+            },
+            {
+                function: "markCheckpoint",
+                address: "%GD + 0x25FB60",
+                args: [
+                    { arg: "-returnValue", type: "unknown", desc: "Unknown" },
+                    { arg: "self", type: "CCLayer*", desc: "PlayLayer" },
+                    { arg: "unknown", type: "void*", desc: "Unknown" },
+                    { arg: "unknown", type: "void*", desc: "Unknown" }
+                ],
+                callingConvention: "__fastcall"
+            },
+            {
+                function: "removeLastCheckpoint",
+                address: "%GD + 0x20B830"
+            },
+            {
+                function: "getCapacityString",
+                address: "%GD + 0x10C9B0"
+            },
+            {
+                function: "updateAttempts",
+                address: "%GD + 0x20CED0"
+            },
+            {
+                function: "resetLevel",
+                address: "%GD + 0x20BF00"
+            },
         ]
     }
 ];
